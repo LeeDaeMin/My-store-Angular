2806d70a10356f53cdac55342478d4ab971c3739
LeeDaeMin <laalmeida@uts.edu.co>
2023-01-13 09:51:18 -0500

    add style standard, style for each component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductosComponent } from './components/productos/productos.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgComponent,
     ProductsComponent,
-    ProductosComponent
+    ProductosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
