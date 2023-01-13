398b56eecdaeabe0654117ce37393abb4c05a2d3
LeeDaeMin <laalmeida@uts.edu.co>
2023-01-13 08:44:16 -0500

    add  @Input and contructors

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,26 +1,71 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent  implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  imageDefault = 'https://raw.githubusercontent.com/platzi/angular-componentes/2-step/src/assets/images/default.png';
+  img: string = '';
 
-  @Input() img: string = 'Valor inicial';
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.log('change just img  =>' ,this.img);
+    // code
+  }
+  @Input() alt: string = '';
   @Output() loaded = new EventEmitter<string>();
+  imageDefault = './assets/images/default.png';
+  // counter = 0;
+  // counterFn: number | undefined;
 
-  ngOnInit(): void{
+  constructor() {
+    // before render
+    // NO async -- once time
+    console.log('constructor', 'imgValue =>', this.img);
   }
 
-  imgError(){
-    this.img = this.imageDefault
+  ngOnChanges(changes: SimpleChanges) {
+    // before - during render
+    // changes inputs -- multiples times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+    console.log('changes', changes);
+    // if (changes.) {
+    //   // code
+    // }
   }
 
-  loadLoaded(){
-    console.log('log from child');
+  ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+    // this.counterFn = window.setInterval(() => {
+    //   this.counter += 1;
+    //   console.log('run counter');
+    // }, 1000);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children -- once time
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    // delete -- once time
+    console.log('ngOnDestroy');
+    // window.clearInterval(this.counterFn);
+  }
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('log hijo');
     this.loaded.emit(this.img);
   }
+
 }
